bcd38dc320b3b9c70c93eaef57bd88ae790f48c3
Guilherme Castro <45200933+cguiama@users.noreply.github.com>
2025-02-21 00:26:40 -0300

    Inicio do projeto

---
+++ JavaSprintBoot/PrimeirosPassos/primeirosPassos/src/main/java/dio/primeirospassos/Calculadora.java
@@ -0,0 +1,22 @@
+package dio.primeirospassos;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculadora {
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public int dividir(int a, int b) {
+        return a / b;
+    }
+}
